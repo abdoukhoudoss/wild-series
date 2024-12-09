ff333227c93bf10e93688aa6b3f6a4a5227ede30
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-12-09 17:26:19 +0100

    monorepo quest4

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,65 @@
+// Some data to make the trick
+
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+import type { RequestHandler } from "express";
+const browse: RequestHandler = (req, res) => {
+  res.json(categories);
+};
+const read: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+  const category = categories.find((c) => c.id === parsedId);
+  if (category) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+const create: RequestHandler = (req, res) => {
+  const newId = categories.length + 1;
+  const newCategory = { id: newId, name: req.body.name };
+  categories.push(newCategory);
+  res.status(201).json(newCategory);
+};
+
+const update: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+  const index = categories.findIndex((c) => c.id === parsedId);
+
+  if (index !== -1) {
+    categories[index] = { ...categories[index], ...req.body };
+    res.json(categories[index]);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+const destroy: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+  const index = categories.findIndex((c) => c.id === parsedId);
+
+  if (index !== -1) {
+    categories.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+export default {
+  browse,
+  read,
+  create,
+  update,
+  destroy,
+};
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import categoryActions from "./modules/category/categoryActions";
 // Import actions
 import itemActions from "./modules/item/itemActions";
 import programActions from "./modules/program/programActions";
@@ -12,6 +13,9 @@ const router = express.Router();
 /* ************************************************************************* */
 
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 // Items routes
 router.get("/api/items", itemActions.browse);
